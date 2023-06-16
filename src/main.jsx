5025bfcf88e911471b850fa949dcdd1609665b09
Md Tuhin Al Jobayer <tuhinjobayergolap007@gmail.com>
2023-06-16 11:22:46 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom'
+import Main from './LayOut/Main.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
